0bb6e5260b1338abe20b666192f34d7f9b2c3017
Matthew Berger <41124308+bergermaestro@users.noreply.github.com>
2025-06-21 22:50:42 -0400

    cleanup pr

--- src/types/masonry-layout.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "masonry-layout" {
-  export default class Masonry {
-    constructor(element: Element | string, options?: any);
-    layout(): void;
-    destroy(): void;
-    reloadItems(): void;
-  }
-}
